acdd9adf11b09d9f4711f7d232ac64b134dcb74e
laboratorio <laboratorio@UPEAULA-31704>
2024-05-20 08:39:29 -0500

    Agregar archivos de proyecto.

---
+++ NotesGabrielPadilla/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace NotesGabrielPadilla
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ NotesGabrielPadilla/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace NotesGabrielPadilla
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
